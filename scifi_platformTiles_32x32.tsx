0f32a52afd0bc4e15fa7e5f84c9470746b7a98dc
JTheilmann <JTheilmann@users.noreply.github.com>
2020-12-04 17:34:14 +0100

    Add files via upload

---
+++ scifi_platformTiles_32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="scifi_platformTiles_32x32" tilewidth="32" tileheight="32" tilecount="805" columns="35">
+ <image source="../../Downloads/scifi_platformTiles_32x32.png" width="1120" height="736"/>
+</tileset>
